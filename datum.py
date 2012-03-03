ab196a9289930e682c633adaad446588b788b648
rymdhund <rymdhund@forskningsavd.se>
2012-03-03 12:42:35 +0100

    add some verbosity

--- datum.py
+++ datum.py
@@ -9,8 +9,18 @@ if len(sys.argv) != 4:
     exit(0)
 c = calendar.Calendar()
 
+day_names = ['mon','tue','wed','thu','fri','sat','sun']
+month_names = ['error','jan','feb','mar','apr','may','june','july','aug','sept',
+        'oct','nov','dec']
+year = int(sys.argv[1])
+month = int(sys.argv[2])
 days = map(int,sys.argv[3].split(","))
-for x,y in c.itermonthdays2(int(sys.argv[1]), int(sys.argv[2])):
+
+
+print 'showing all '+str(map(lambda x: day_names[x],days))+" in "\
+        +month_names[month]+" of "+str(year)+":"
+
+for x,y in c.itermonthdays2(year, month):
     if x > 0:
         if y in days:
             print x
